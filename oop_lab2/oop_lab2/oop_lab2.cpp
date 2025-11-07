b70a7317528004c3c613ad579a3dd44df8e9ee5a
marywave <marissdddddd@gmail.com>
2025-11-07 20:09:20 +0500

    Made class Point

--- oop_lab2/oop_lab2/oop_lab2.cpp
+++ oop_lab2/oop_lab2/oop_lab2.cpp
@@ -1,7 +1,35 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+
+using namespace std;
+
+class Point {
+public:
+    int x, y;
+    Point() { // default constructor
+        cout << "[Point] default\n";
+        x = 0;
+        y = 0;
+    }
+    Point(int x,int y) { //constructor with parametr
+        cout << "[Point] const. param.\n";
+        this -> x = x;
+        this -> y = y;
+    }
+    Point(const Point &p) { //copy constructor
+        cout << "[Point] copy\n";
+        x = p.x;
+        y = p.y;
+    }
+    ~Point() { cout << "[~Point]\n"; }
+};
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Point p;
+    Point p2(1, 2);
+    Point p3(p2);
+    cout << "Hello World!\n";
 }
