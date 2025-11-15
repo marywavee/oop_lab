fc1aba0d3abc75eb1e57e7d6cefd9f0af2020dd4
marywavee <marissdddddd@gmail.com>
2025-11-15 13:06:12 +0500

    made MainWindow

--- oop_lab3.2/main.py
+++ oop_lab3.2/main.py
@@ -1,1 +1,30 @@
-print("bbb")
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+class Model:
+    def __init__(self):
+        pass
+
+class MainWindow(QMainWindow):
+    def __init__(self, model: Model):
+        super().__init__()
+
+        self.setStyleSheet("background-color: white;")
+        self.model = model
+        self.setWindowTitle("mainWindow")
+        self.resize(500, 300)
+
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    model = Model()
+    window = MainWindow(model)
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
